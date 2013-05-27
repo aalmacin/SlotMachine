bb48951e491d4695c8dbe86363639f90b677df62
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2013-05-27 11:18:57 -0400

    Added icon_image in icon class

--- slotmachine0_2.py
+++ slotmachine0_2.py
@@ -21,8 +21,8 @@ def main():
 class SlotMachine:
   def __init__(self, starting_jackpot, starting_cash):
     self.POSSIBLE_BETS = "10, 20, 50, 100"
-    self.symbols = []
-    self.__create_symbols()
+    self.icons = []
+    self.__create_icons()
     self.JACKPOT_INCREASE_RATE = .15
     self.FRAME_RATE = 60
 
@@ -40,15 +40,15 @@ class SlotMachine:
     self.wins = 0
     self.loses = 0
 
-  def __create_symbols(self):
-    self.symbols.append(Symbol("Blank", 0, 0, 2))
-    self.symbols.append(Symbol("Grapes", 20, 2))
-    self.symbols.append(Symbol("Banana", 30, 2))
-    self.symbols.append(Symbol("Orange", 40, 3))
-    self.symbols.append(Symbol("Cherry", 100, 4))
-    self.symbols.append(Symbol("Bar", 200, 5))
-    self.symbols.append(Symbol("Bell", 300, 10))
-    self.symbols.append(Symbol("Seven", 1000, 20, 10))
+  def __create_icons(self):
+    self.icons.append(Icon("Blank", 0, 0, "sadface.png", bonus_win_rate = 2))
+    self.icons.append(Icon("Grapes", 20, 2, "katipunero_hat.png"))
+    self.icons.append(Icon("Banana", 30, 2, "bandana.png"))
+    self.icons.append(Icon("Orange", 40, 3, "camesa_de_chino.png"))
+    self.icons.append(Icon("Cherry", 100, 4, "banyal.png"))
+    self.icons.append(Icon("Bar", 200, 5, "tsinelas.png"))
+    self.icons.append(Icon("Bell", 300, 10, "arnis.png"))
+    self.icons.append(Icon("Seven", 1000, 20, "seven.png", bonus_win_rate = 10))
 
   def start_game(self):
     # Assign the Display Variables
@@ -95,21 +95,21 @@ class SlotMachine:
       spinned_result = random.randint(0, 100)
 
       if spinned_result in range(0, 40):
-          self.results[spin] = self.symbols[0].name
+          self.results[spin] = self.icons[0].name
       elif spinned_result in range(40, 56):
-          self.results[spin] = self.symbols[1].name
+          self.results[spin] = self.icons[1].name
       elif spinned_result in range(56, 70):
-          self.results[spin] = self.symbols[2].name
+          self.results[spin] = self.icons[2].name
       elif spinned_result in range(70, 82):
-          self.results[spin] = self.symbols[3].name
+          self.results[spin] = self.icons[3].name
       elif spinned_result in range(82, 89):
-          self.results[spin] = self.symbols[4].name
+          self.results[spin] = self.icons[4].name
       elif spinned_result in range(89, 95):
-          self.results[spin] = self.symbols[5].name
+          self.results[spin] = self.icons[5].name
       elif spinned_result in range(95, 99):
-          self.results[spin] = self.symbols[6].name
+          self.results[spin] = self.icons[6].name
       elif spinned_result in range(99, 100):
-          self.results[spin] = self.symbols[7].name
+          self.results[spin] = self.icons[7].name
 
     self.__check_results()
 
@@ -121,17 +121,17 @@ class SlotMachine:
 
   def __check_results(self):
     win = False
-    for symbol in self.symbols:
-      if self.results.count(symbol.name) == 3:
-        self.current_cash += self.bet * symbol.win_rate_full
+    for icon in self.icons:
+      if self.results.count(icon.name) == 3:
+        self.current_cash += self.bet * icon.win_rate_full
         win = True
-      if self.results.count(symbol.name) == 2:
-        self.current_cash += self.bet * symbol.win_rate_two
+      if self.results.count(icon.name) == 2:
+        self.current_cash += self.bet * icon.win_rate_two
         win = True
-    if self.results.count(self.symbols[0].name) == 0 and not win:
-      self.current_cash += self.bet * self.symbols[0].bonus_win_rate
-    if self.results.count(self.symbols[7].name) == 1 and not win:
-      self.current_cash += self.bet * self.symbols[7].bonus_win_rate
+    if self.results.count(self.icons[0].name) == 0 and not win:
+      self.current_cash += self.bet * self.icons[0].bonus_win_rate
+    if self.results.count(self.icons[7].name) == 1 and not win:
+      self.current_cash += self.bet * self.icons[7].bonus_win_rate
 
     #TODO Jackpot
 
@@ -144,8 +144,8 @@ class SlotMachine:
   def reset(self):
     self.set_initial_values()
 
-class Symbol:
-  def __init__(self, name, win_rate_full, win_rate_two, bonus_win_rate = 0):
+class Icon:
+  def __init__(self, name, win_rate_full, win_rate_two, icon_image, bonus_win_rate = 0):
     self.name = name
     self.win_rate_full = win_rate_full
     self.win_rate_two = win_rate_two
@@ -163,12 +163,7 @@ class Symbol:
   def get_name():
     return self.name
 
-class GameBackground(pygame.sprite.Sprite):
-  def __init__(self):
-    self.image = pygame.image.load("background.png")
-
-  def get_size(self):
-    self.image.get_size()
-
+  def get_image():
+    return self.icon_image
 
 if __name__ == "__main__": main()
