616f2473582e5f447505a143cca631c0265e5298
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2013-05-27 15:50:20 -0400

    Added buttons in screen

--- slotmachine0_3.py
+++ slotmachine0_3.py
@@ -18,6 +18,13 @@ FRAME_RATE = 30
 GAME_TITLE = "Slot Machine"
 BACKGROUND_IMAGE_NAME = "background.png"
 
+class SlotMachineButton(pygame.sprite.Sprite):
+  def __init__(self, image_name, value):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.image.load(image_name)
+    self.image = self.image.convert()
+    self.rect = self.image.get_rect()
+    self.value = value
 
 def start_game():
   # Assign the Display Variables
@@ -25,16 +32,33 @@ def start_game():
   screen = pygame.display.set_mode(background.get_size())
   pygame.display.set_caption(GAME_TITLE)
 
+  ten_button = SlotMachineButton("ten_button.png", 10)
+  twenty_button = SlotMachineButton("twenty_button.png", 20)
+  fifty_button = SlotMachineButton("fifty_button.png", 50)
+  hundred_button = SlotMachineButton("hundred_button.png", 100)
+
+  button_sprites = pygame.sprite.Group(ten_button, twenty_button, fifty_button, hundred_button)
+
   clock = pygame.time.Clock()
 
   continue_playing = True
   while (continue_playing):
+    # Tick
     clock.tick(FRAME_RATE)
+
     for event in pygame.event.get():
       if (event.type == pygame.QUIT):
         continue_playing = False
 
+    BUTTON_BOTTOM_POS = background.get_height() - ten_button.image.get_height() - 50
+
     screen.blit(background, background.get_rect())
+
+    x = 30
+    for btn_sprite in button_sprites:
+      screen.blit(btn_sprite.image, (x, BUTTON_BOTTOM_POS))
+      x += (btn_sprite.image.get_width() + 30)
+
     pygame.display.flip()
 
 def main():
